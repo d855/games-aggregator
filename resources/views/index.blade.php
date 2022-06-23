19d4adc644229bbc11bc19ef8ce60cfceebaf480
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2022-06-23 23:11:03 +0200

    index page design and responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,88 @@
 
 
 @section('content')
-	index here
+	<div class="container mx-auto px-4">
+		<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular games</h2>
+
+		<div class="popular-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+			@foreach(range(1, 12) as $item)
+				<div class="game mt-8">
+					<div class="relative inline-block">
+						<a href="#">
+							<img src="https://nerdiertides.files.wordpress.com/2016/07/watch-dogs-ubisoft-cover-art.jpg"
+							     alt="game cover"
+							     class="hover:opacity-75 transition ease-in-out duration-150">
+						</a>
+						<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
+						     style="right: -20px; bottom: -20px;">
+							<div class="font-semibold text-xs flex justify-center items-center h-full">
+								80%
+							</div>
+						</div>
+					</div>
+
+					<a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+						Watch dogs
+					</a>
+					<div class="text-gray-400 mt-1">Playstation 4</div>
+				</div>
+			@endforeach
+		</div><!-- end popular games -->
+
+		<div class="flex flex-col lg:flex-row my-10">
+			<div class="recently-reviewed w-full mr-0 lg:w-3/4 lg:mr-32">
+				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+				<div class="recently-reviewed-container space-y-12 mt-8">
+					@foreach(range(1, 3) as $item)
+						<div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+							<div class="relative flex-none">
+								<a href="#">
+									<img src="https://nerdiertides.files.wordpress.com/2016/07/watch-dogs-ubisoft-cover-art.jpg"
+									     alt="game cover"
+									     class="hover:opacity-75 transition w-48 ease-in-out duration-150">
+								</a>
+								<div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full"
+								     style="right: -20px; bottom: -20px;">
+									<div class="font-semibold text-xs flex justify-center items-center h-full">
+										80%
+									</div>
+								</div>
+							</div>
+							<div class="ml-12">
+								<a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+									Watch dogs
+								</a>
+								<div class="text-gray-400 mt-1">Playstation 4</div>
+
+								<p class="mt-6 text-gray-400 hidden lg:block">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ad adipisci alias
+									aliquid cumque dicta eaque et ex fugit iste molestias natus odio quae quisquam ratione
+									repellendus tempora velit voluptas.
+								</p>
+							</div>
+						</div>
+					@endforeach
+				</div>
+			</div>
+			<div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
+				<h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+				<div class="most-anticipated-container space-y-10 mt-8">
+					@foreach(range(1, 4) as $item)
+						<div class="game flex">
+							<a href="#">
+								<img src="https://nerdiertides.files.wordpress.com/2016/07/watch-dogs-ubisoft-cover-art.jpg"
+								     alt="game cover"
+								     class="hover:opacity-75 w-16 transition ease-in-out duration-150">
+							</a>
+
+							<div class="ml-4">
+								<a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+								<p class="text-gray-400 text-sm mt-1">Sept 16 2022</p>
+							</div>
+						</div>
+					@endforeach
+				</div>
+			</div>
+		</div>
+	</div><!-- end container -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,17 +10,42 @@
 	</head>
 	<body class="bg-gray-900 text-white">
 		<header class="border-b border-gray-800">
-			<nav class="container mx-auto flex items-center justify-between px-4 py-6">
-				<div class="flex items-center">
+			<nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+				<div class="flex flex-col lg:flex-row items-center">
 					<a href="/">Logo</a>
 
-					<ul class="flex items-center">
+					<ul class="flex items-center lg:ml-16 space-x-3 mt-6 lg:mt-0">
 						<li><a href="#" class="hover:text-gray-400">Games</a></li>
 						<li><a href="#" class="hover:text-gray-400">Reviews</a></li>
 						<li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
 					</ul>
 				</div>
+
+				<div class="flex items-center mt-6 lg:mt-0">
+					<div class="relative">
+						<input type="text" class="bg-gray-800 pl-8 text-sm rounded-full w-64 px-3 py-1 focus:outline-none focus:shadow-outline" placeholder="Search">
+						<div class="absolute top-0 flex items-center h-full ml-2">
+							<svg class="text-gray-400 w-4" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+							</svg>
+						</div>
+					</div>
+
+					<div class="ml-6">
+						<a href="#"><img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y" alt="avatar" class="rounded-full w-8"></a>
+					</div>
+				</div>
 			</nav>
 		</header>
+
+		<main class="py-8">
+			@yield('content')
+		</main>
+
+		<footer class="border-t border-gray-800">
+			<div class="container mx-auto px-4 py-6 text-xs">
+				Created by d855 with help of Laracast and <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+			</div>
+		</footer>
 	</body>
 </html>
