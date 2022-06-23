abfc85b2cfaeb29effd84d0979ea6fe6068fe32b
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2022-06-23 21:45:37 +0200

    set up

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+		      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Games aggregator</title>
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	</head>
+	<body class="bg-gray-900 text-white">
+		<header class="border-b border-gray-800">
+			<nav class="container mx-auto flex items-center justify-between px-4 py-6">
+				<div class="flex items-center">
+					<a href="/">Logo</a>
+
+					<ul class="flex items-center">
+						<li><a href="#" class="hover:text-gray-400">Games</a></li>
+						<li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+						<li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	</body>
+</html>
